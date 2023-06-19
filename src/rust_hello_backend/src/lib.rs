fc7bb5ed7aca070ba9c5383544cb28f7a2782550
Jian Earle <jianearle93@googlemail.com>
2023-06-19 23:20:19 +0100

    v1 - Skeleton backend methods

--- src/rust_hello_backend/src/lib.rs
+++ src/rust_hello_backend/src/lib.rs
@@ -2,3 +2,33 @@
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+/* Should probably have separate canisters for
+merchant functions and user TBC on design
+ */
+
+// User functions
+#[ic_cdk::query]
+fn add_to_cart() -> String {
+    format!("Added product to cart")
+}
+
+#[ic_cdk::query]
+fn delete_from_cart() -> String {
+    format!("Remove product from cart")
+}
+
+#[ic_cdk::query]
+fn add_product() -> String {
+    format!("Remove product from cart")
+}
+
+// Merchant functions
+#[ic_cdk::query]
+fn remove_product() -> String {
+    format!("Listed product on de-cart")
+}
+
+#[ic_cdk::query]
+fn import_products() -> String {
+    format!("Fetched products from e-commerce platform")
+}
