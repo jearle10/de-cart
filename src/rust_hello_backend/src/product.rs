c2c2a0685ef4ece3984e1f30138e2d451b2a4c66
Jian Earle <jianearle93@googlemail.com>
2023-06-24 16:22:28 +0100

    Added https outcall to fetch products from cscart

--- src/rust_hello_backend/src/product.rs
+++ src/rust_hello_backend/src/product.rs
@@ -1,4 +1,13 @@
 use ic_cdk::export::candid::{ candid_method, Deserialize, CandidType };
+use ic_cdk::api::management_canister::http_request::{
+    http_request, CanisterHttpRequestArgument, HttpHeader,
+    HttpMethod, HttpResponse, TransformArgs,
+    TransformContext,
+};
+
+use base64;
+use base64::Engine;
+use ic_cdk::api::call::CallResult;
 
 #[derive(Clone, Debug, Default, Deserialize, CandidType)]
 struct Product {
@@ -20,8 +29,50 @@ fn remove_product() -> String {
     format!("Removed product on de-cart")
 }
 
-#[ic_cdk::query]
-#[candid_method(query)]
-fn get_cscart_products() -> String {
+#[ic_cdk::update]
+#[candid_method(update)]
+async fn get_cscart_products(
+    username : String,
+    password : String,
+    host : String,
+) -> String {
+
+    // Construct basic auth token using credentials
+    let credentials = format!("{}:{}", username , password);
+    let engine = base64::engine::general_purpose::STANDARD;
+    let token = engine.encode(credentials.as_bytes());
+
+    let url = format!("https://{}/api/2.0/products", host);
+
+    let request_headers = vec![
+        HttpHeader {
+            name: "Host".to_string(),
+            value: format!("{host}:443"),
+        },
+        HttpHeader {
+            name: "Authorization".to_string(),
+            value: format!("Bearer {token}"),
+        },
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url,
+        max_response_bytes: None, //optional for request
+        method: HttpMethod::GET,
+        headers: request_headers,
+        body: None,      //optional for request
+        transform: None, //optional for request
+    };
+
+    match http_request(request).await {
+        Ok((response,)) => {
+            let body = String::from_utf8(response.body)
+                .expect("Invalid utf8 string received");
+            ic_cdk::println!("{:?}", body)
+        },
+        Err(e) => ic_cdk::println!("{:?}", e),
+    }
+
+    // ic_cdk::println!("Testing printing logs");
     format!("Fetched cscart products")
 }
