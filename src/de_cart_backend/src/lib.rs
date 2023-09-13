8fd7079714f8500785475cd03a338e9d3ae170f3
Jian Earle <jianearle93@googlemail.com>
2023-09-13 16:25:18 +0100

    Remove redundant .did generation workaround test

--- src/de_cart_backend/src/lib.rs
+++ src/de_cart_backend/src/lib.rs
@@ -23,24 +23,3 @@ thread_local!{
 }
 
 export_candid!();
-
-#[test]
-fn check_candid_interface() {
-    use candid::utils::{service_compatible, CandidSource };
-    use std::path::Path;
-    
-
-    // Generate the service definition (candid format)
-    candid::export_service!();
-    let new_interface = __export_service();
-
-    // Write the generated service definition to a file for dfx deploy
-    let buffer = new_interface.as_bytes();
-    std::fs::write("backend.did", buffer)
-        .expect("Issue generating backend.did file");
-
-    service_compatible(
-        CandidSource::Text(&new_interface),
-        CandidSource::File(Path::new("backend.did")),
-    ).unwrap();
-}
