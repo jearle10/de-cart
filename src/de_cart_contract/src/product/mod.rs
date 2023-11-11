f0b8cae91f5c22099478672730ba8258b8ff8d03
Jian Earle <jianearle93@googlemail.com>
2023-11-11 16:38:23 +0000

    tests added for product module

--- src/de_cart_contract/src/product/mod.rs
+++ src/de_cart_contract/src/product/mod.rs
@@ -1,9 +1,10 @@
 use candid::{ Deserialize, CandidType };
 use std::collections::HashMap;
+
 #[derive(Clone, Debug, Deserialize, CandidType)]
 pub struct Product {
     pub (crate) sku: String,
-    pub (crate) merchant_id : String,
+    pub (crate) merchant_id : MerchantId,
     pub (crate) product_id : String,
     pub (crate) name: String,
     pub (crate) price: candid::Nat,
@@ -25,9 +26,12 @@ impl Default for Product {
     }
 }
 
+type MerchantId = String;
+type ProductId = String;
+
 #[derive(Deserialize, Debug, CandidType, Default, Clone)]
 pub struct ProductStore {
-    pub (crate) products : HashMap<String, HashMap<String, Product>>
+    pub (crate) products : HashMap<MerchantId, HashMap<ProductId, Product>>
 }
 
 
@@ -72,4 +76,95 @@ impl ProductStore {
     pub fn add_merchant(&mut self , merchant_id : String){
         self.products.insert(merchant_id.to_string(), HashMap::new());
     }
+
+    fn get_merchant_store(&self , merchant_id : String) -> Option<(&ProductId, &HashMap<ProductId, Product>)>{
+        self.products.get_key_value(&merchant_id)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::product;
+
+    use super::*;
+
+    #[test]
+    fn it_creates_new_merchant_store(){
+
+        let mut store = ProductStore::new();
+        let merchant_id = "one".to_string();
+        
+        store.add_merchant(merchant_id.clone());
+        let merchant_store = store.get_merchant_store(merchant_id.clone()).unwrap();
+        assert_eq!(&merchant_id, merchant_store.0);
+    }
+
+    #[test]
+    fn it_adds_a_product(){
+
+        let mut store = ProductStore::new();
+        let merchant_id = "one".to_string();
+
+        let new_product = product::Product {
+            merchant_id : merchant_id.clone(),
+            sku: "my_sku".to_string(),
+            ..Product::default()
+        };
+        
+        store.add_merchant(merchant_id.clone()); // Setup a store for the merchant first
+        store.add(merchant_id.clone(), new_product);
+
+        let merchant_store = store.products.get(&merchant_id).cloned().unwrap();
+        let product = merchant_store.get("my_sku").cloned().unwrap();
+        assert_eq!("my_sku".to_string(), product.sku);
+    }
+
+
+    #[test]
+    fn it_gets_a_product(){
+
+        let mut store = ProductStore::new();
+        let merchant_id = "one".to_string();
+
+        let new_product = product::Product {
+            merchant_id : merchant_id.clone(),
+            sku: "my_sku".to_string(),
+            ..Product::default()
+        };
+        
+        store.add_merchant(merchant_id.clone()); // Setup a store for the merchant first
+        store.add(merchant_id.clone(), new_product);
+
+        let product = store.get(merchant_id, "my_sku".to_string()).unwrap();
+        assert_eq!("my_sku".to_string(), product.sku);
+    }
+
+
+    #[test]
+    fn it_deletes_a_product(){
+
+        let mut store = ProductStore::new();
+        let merchant_id = "one".to_string();
+
+        let new_product = product::Product {
+            merchant_id : merchant_id.clone(),
+            sku: "my_sku".to_string(),
+            ..Product::default()
+        };
+        
+        store.add_merchant(merchant_id.clone()); // Setup a store for the merchant first
+        store.add(merchant_id.clone(), new_product);
+
+        let product = store.get(merchant_id.clone(), "my_sku".to_string()).unwrap();
+
+        assert_eq!("my_sku".to_string() , product.sku);
+
+        store.delete(merchant_id.clone(), "my_sku".to_string());
+
+        let product = store.get(merchant_id, "my_sku".to_string());
+        assert_eq!(true ,product.is_none());
+    }
+
+
 }
